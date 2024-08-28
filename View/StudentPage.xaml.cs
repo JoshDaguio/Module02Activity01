ad22ea00e902837e831a250ce6d6576ee5358912
User <User@B20>
2024-08-28 10:43:20 +0800

    Add project files.

---
+++ View/StudentPage.xaml.cs
@@ -0,0 +1,12 @@
+namespace Module02Activity01.View;
+
+using Module02Activity01.ViewModel;
+
+public partial class StudentPage : ContentPage
+{
+	public StudentPage()
+	{
+		InitializeComponent();
+		BindingContext = new StudentViewModel();
+	}
+}
